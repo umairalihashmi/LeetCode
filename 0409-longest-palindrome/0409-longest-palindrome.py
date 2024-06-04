09716be5bae16064b472cb02202240d5ea69fac9
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-06-04 17:47:48 +0500

    Time: 37 ms (54.97%), Space: 16.6 MB (34.5%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         a = Counter(s)
-        res = odd = modd = 0
+        res = odd = 0
         for i in a:
-            if a[i]%2:
-                odd +=1
-            res+= a[i]
+            if a[i] % 2:
+                odd += 1
+            res += a[i]
         if odd:
             return res - odd +1
-
         return res - odd
 
         
