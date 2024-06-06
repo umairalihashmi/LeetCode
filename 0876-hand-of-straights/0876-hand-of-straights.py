9d7af9aad33b1686b16f6e1e574a3350e62e6266
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-06-06 23:06:42 +0500

    Time: 2331 ms (5.01%), Space: 17.7 MB (99.56%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,7 +1,6 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         hand.sort()
-        # print(hand)
         if len(hand) % groupSize:
             return False
         for i in range((len(hand)//groupSize)+1):
@@ -10,27 +9,19 @@ class Solution:
             t = 0
             while t < len(hand) and hand[t] == -1:
                 t+=1
-            # print("t went to :",t)
-            # if hand[-1] == -1:
-            #     break
             temp = []
             for i in range(t,len(hand)):
-                # print("index:",i,temp,hand)
                 if len(temp) == groupSize:
                     break
-                if temp and temp[-1] == hand[i]:
+                elif temp and temp[-1] == hand[i]:
                     continue
-                if hand[i] == -1:
+                elif hand[i] == -1:
                     continue
-                if temp and temp[-1] != hand[i]-1:
-                    # print(temp," and not consecutive :",temp[-1],hand[i]-1)
+                elif temp and temp[-1] != hand[i]-1:
                     continue
-                
                 temp.append(hand[i])
                 hand[i] = -1
-                # print("hand:",hand)
             if len(temp) < groupSize:
-                # print(len(temp),"< gsize")
                 return False
         for i in hand:
             if hand[i] > -1:
