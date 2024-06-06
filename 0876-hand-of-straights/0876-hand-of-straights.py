67fce4591f57a661a27607203a4650e31a5d6869
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-06-06 23:04:48 +0500

    Time: 2320 ms (5.01%), Space: 18 MB (94.17%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,23 +1,38 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        count = Counter(hand)
-        arr = sorted(count.keys())
-        while sum(count.values()):
+        hand.sort()
+        # print(hand)
+        if len(hand) % groupSize:
+            return False
+        for i in range((len(hand)//groupSize)+1):
+            if 0 <= len(hand)-hand.count(-1) < groupSize:
+                break 
+            t = 0
+            while t < len(hand) and hand[t] == -1:
+                t+=1
+            # print("t went to :",t)
+            # if hand[-1] == -1:
+            #     break
             temp = []
-            for i in arr:
-                if i in count:
-                    if count[i] > 0:
-                        if temp and i != temp[-1]+1:
-                            return False
-                        count[i]-=1
-                        temp.append(i)
-                    else:
-                        continue 
+            for i in range(t,len(hand)):
+                # print("index:",i,temp,hand)
                 if len(temp) == groupSize:
                     break
+                if temp and temp[-1] == hand[i]:
+                    continue
+                if hand[i] == -1:
+                    continue
+                if temp and temp[-1] != hand[i]-1:
+                    # print(temp," and not consecutive :",temp[-1],hand[i]-1)
+                    continue
+                
+                temp.append(hand[i])
+                hand[i] = -1
+                # print("hand:",hand)
             if len(temp) < groupSize:
+                # print(len(temp),"< gsize")
+                return False
+        for i in hand:
+            if hand[i] > -1:
                 return False
         return True
-
-
-        
