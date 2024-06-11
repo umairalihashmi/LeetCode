34ed8be7296f6a4d908a73f028cb41642bd7e293
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-06-11 15:04:00 +0500

    Time: 39 ms (68.91%), Space: 16.8 MB (30.54%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        keycount = Counter(arr1)
+        res = []
+        for item in arr2:
+            res.extend([item for _ in range(keycount[item])])
+            del(keycount[item])
+        remaining = sorted(list(keycount.keys()))
+        for item in remaining:
+            res.extend([item for _ in range(keycount[item])])
+
+        return res
+        
