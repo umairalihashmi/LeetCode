89e9827067975fbd5930fe57f4304ba0f8e815bc
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-06-29 02:18:02 +0500

    Time: 1311 ms (27.82%), Space: 43 MB (27.82%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -5,10 +5,10 @@ class Solution:
         for road in roads:
             counter[road[1]] += 1
             counter[road[0]] += 1
-        x = sorted(counter.items(), key=lambda item: item[1], reverse = True)
-        map = { x[i][0] : n-i for i in range(len(x)) }
+        x = sorted( [[counter[key],key] for key in counter], reverse = True )
+        map = { x[i][1] : n-i for i in range(len(x)) }
         for link in roads:
-            result += (map[link[0]] + map[link[1]])
+            result = result + map[link[0]] + map[link[1]]
         return result
                 
         
