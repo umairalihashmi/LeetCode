fd163228ea6d48196d4983e3c91b1b900a42029e
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-07-10 01:55:00 +0500

    Time: 789 ms (15.46%), Space: 57 MB (85.22%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -1,11 +1,10 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
         total = 0
-        earlier = customers[0][0]
+        start = customers[0][0]
         for order in customers:
-            earlier = max(earlier,order[0])
-            earlier+=order[1]
-            total = total + earlier - order[0]
-        
+            start = max(start,order[0])
+            start += order[1]
+            total = total + start - order[0]
         return total/len(customers)
         
