fd268dae6a326453c32248808b0ddcacc74d3765
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-07-18 01:09:01 +0500

    Time: 53 ms (68.79%), Space: 17.1 MB (7.44%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -8,39 +8,28 @@ class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
         ans = []
         delete = set(to_delete)
-        
-
         def trav(head):
             if not head:
-                print("leaf")
                 return False
             if head.val in delete:
-                print(head.val,"in delete")
                 if head.left:
                     if head.left.val not in delete:
                         ans.append(head.left)
-                    print("sending below left from",head.val)
                     if trav(head.left):
                         head.left = None
                 if head.right:
                     if head.right.val not in delete:
                         ans.append(head.right)
-                    print("sending below right from",head.val)
                     if trav(head.right):
                         head.right = None
                 return True 
             else:
-                print("sending below left from non delete",head.val)
                 if trav(head.left):
-                    print(head.left.val,"in delete")
                     head.left = None
-                print("sending below right from non delete",head.val)
                 if trav(head.right):
-                    print(head.right.val,"in delete")
                     head.right = None
             return False
         trav(root)
         if root.val not in delete:
             ans.append(root)
-
         return ans
