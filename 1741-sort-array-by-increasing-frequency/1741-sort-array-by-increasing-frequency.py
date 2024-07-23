f93dce4fbccb8a9bc421e504a00027301ae1de1a
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-07-24 02:34:42 +0500

    Time: 48 ms (67.92%), Space: 16.8 MB (12.65%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
@@ -1,16 +1,12 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         x = Counter(nums)
-        res = []
-        # print(x)
         partmap = defaultdict(list)
         for item in x:
             partmap[x[item]].extend([item]*x[item])
-        # print(partmap)
-        k = sorted(partmap.keys())
-        for keys in k:
-            partmap[keys].sort(reverse = True)
-            res.extend(partmap[keys])
-
+        res = []
+        keys = sorted(partmap.keys())
+        for k in keys:
+            res.extend(sorted(partmap[k],reverse = True))
         return res
         
