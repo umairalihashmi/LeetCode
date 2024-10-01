ac37d6fc13cd768eb48dffe3851702fab9c9234f
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-02 01:37:47 +0500

    Time: 527 ms (26.81%), Space: 30.4 MB (74.83%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -1,9 +1,11 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        modarr = [ val%k for val in arr]
-        print(modarr)
-        mapp = Counter(modarr)
-        print(mapp)
+        mapp = defaultdict(int)
+        modarr = []
+        for val in arr:
+            md = val%k
+            modarr.append(md)
+            mapp[md] += 1
         if 0 in mapp and mapp[0]%2:
             return False
         for item in modarr:
