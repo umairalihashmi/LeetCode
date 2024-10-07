54096a3b7c6d40dfc68358ef4c76c5fee6b8ad56
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-08 01:27:20 +0500

    Time: 50 ms (23.86%), Space: 16.6 MB (42%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -2,9 +2,17 @@ class Solution:
     def minLength(self, s: str) -> int:
         stack = []
         for i in s:
-            stack.append(i)
-            while len(stack)>= 2 and (stack[-2:] == ["A","B"] or stack[-2:] == ["C","D"]):
-                stack.pop()
-                stack.pop()
+            if i == "B":
+                if stack and stack[-1] == "A":
+                    stack.pop()
+                else:
+                    stack.append(i)
+            elif i == "D":
+                if stack and stack[-1] == "C":
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
         return len(stack)
         
