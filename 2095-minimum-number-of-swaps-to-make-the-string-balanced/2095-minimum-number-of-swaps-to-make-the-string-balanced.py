4f957c9fc1a8f1b1902606d60e413b95862103f1
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-08 20:23:27 +0500

    Time: 215 ms (59.41%), Space: 29.3 MB (19.6%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -3,7 +3,6 @@ class Solution:
         stack = []
         swap = 0
         for bracket in s:
-            # print("bracket:",bracket,stack)
             if bracket == "]":
                 if not stack:
                     stack.append("[")
@@ -12,7 +11,4 @@ class Solution:
                     stack.pop()
             else:
                 stack.append(bracket)
-        #     print("after bracket",stack)
-        # print(stack,swap)
-        # print(swap+(len(stack)//2))
         return swap
