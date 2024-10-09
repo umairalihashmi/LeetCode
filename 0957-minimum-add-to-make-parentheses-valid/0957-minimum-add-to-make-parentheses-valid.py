a96896debadea9c10171bdae86a7657bf99f13c7
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-09 16:35:21 +0500

    Time: 41 ms (16.27%), Space: 16.6 MB (30.64%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -4,10 +4,7 @@ class Solution:
         stk = []
         for b in s:
             if b == ")":
-                if stk and stk[-1] == "(":
-                    stk.pop()
-                else:
-                    stk.append(b)
+                stk.pop() if stk and stk[-1] == "(" else stk.append(b)
             else:
                 stk.append(b)
         return len(stk)
