070f928068158d1538d8acc24b29ec99e685ae66
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-09 16:34:34 +0500

    Time: 49 ms (7.09%), Space: 16.6 MB (30.64%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -1,16 +1,14 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
                 
-        stk, count = [], 0
+        stk = []
         for b in s:
             if b == ")":
-                if not stk:
-                    count = count + 1
-                elif stk[-1] == "(":
+                if stk and stk[-1] == "(":
                     stk.pop()
                 else:
                     stk.append(b)
             else:
                 stk.append(b)
-        return len(stk) + count
+        return len(stk)
         
