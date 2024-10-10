648d5bcb3985c9360f7d6975bbba6f8657d80265
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-11 03:11:42 +0500

    Time: 361 ms (36.8%), Space: 23.9 MB (29.11%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.py
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
         mon, length, prev = [], 0, float('inf')
-
         for i,n in enumerate(nums):
             if n < prev:
                 mon.append(i)
                 prev = n
         for i in range(len(nums)-1,-1,-1):
+            if not mon:
+                break
             while mon and nums[mon[-1]] <= nums[i]:
                 length = max(length,(i-mon[-1]))
                 mon.pop()
