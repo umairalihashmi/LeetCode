9e71e15c455f235f977c95f258e7b8eb1ad570ac
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-11 03:12:53 +0500

    Time: 353 ms (45.1%), Space: 23.7 MB (57.16%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.py
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.py
@@ -6,8 +6,6 @@ class Solution:
                 mon.append(i)
                 prev = n
         for i in range(len(nums)-1,-1,-1):
-            if not mon:
-                break
             while mon and nums[mon[-1]] <= nums[i]:
                 length = max(length,(i-mon[-1]))
                 mon.pop()
