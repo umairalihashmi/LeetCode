22c1e8d55810893ca787b267f83fe73a35b24476
umairalihashmi <130283884+umairalihashmi@users.noreply.github.com>
2024-10-18 16:55:46 +0500

    Time: 311 ms (50.21%), Space: 16.8 MB (32.63%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,18 +1,14 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        orr = reduce(lambda x, y: x | y, nums)
-        count = 0
+        count, orr = 0,  reduce(lambda x, y: x | y, nums)
         def trav(arr, our):
             nonlocal count
             if not arr:
-                if our == orr:
-                    count+=1
+                count = count+1 if our == orr else count
                 return
             trav(arr[1:],our)
             our = our | arr[0]
             trav(arr[1:],our)
-
         trav(nums,count)
-            
         return count
         
